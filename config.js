c59c8e77eae4d6a07ed8231e24cef4784ba73405
amq-tutti <tutti-amq@proton.me>
2025-08-07 20:00:06 +0300

    Bump localsotrage prefix

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    localStoragePrefix: "2012-anime-songs-pr",
+    localStoragePrefix: "2012-anime-songs-pr-v2",
     title: "2012 Anime Songs",
     description: "Party rank sorter for nominated anime songs from 2012"
 };
